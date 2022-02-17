b63d78bee3c19ef19c190e2c60720f5a622412f7
Vishal Grover <vishal_grover@crgl-thirdparty.com>
2022-02-17 14:51:58 +0530

    Added basic set-up for the application

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,6 +1,13 @@
+import { Container } from "@mui/material";
+import { alpha, styled } from "@mui/material/styles";
 
+const LoginPageContainer = styled(Container)({
+  backgroundColor: "#E5E5E5",
+  width: "100vw",
+  height: "100vh",
+});
 const Login = () => {
-  return <div>Login</div>;
+  return <LoginPageContainer>Login</LoginPageContainer>;
 };
 
 export default Login;
