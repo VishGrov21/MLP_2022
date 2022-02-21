676fcac33c48be8cdbd89ff48989c5c6815a2e3c
Pavithra <pavithra_muthumanickam@crgl-thirdparty.com>
2022-02-21 10:31:58 +0530

    Adding page layout code

---
+++ src/pages/Layout.page.tsx
@@ -0,0 +1,26 @@
+import { Header } from './Header.page';
+import SideNav from './SideNav.page';
+import { Footer } from './Footer.page';
+import { styled,Stack } from "@mui/material"
+
+const LayoutContainer = styled(Stack)({
+    flexDirection: 'row'
+});
+
+interface layoutTypes {
+    children: object
+}
+
+export const Layout = (props:layoutTypes) => {
+    const { children } = props;
+    return (
+        <LayoutContainer>
+            <SideNav/>
+            <div>
+                <Header/>
+                    <div>{children}</div>
+                <Footer/>
+            </div>
+        </LayoutContainer>
+    )
+}
--- src/routes/RootNavigator.tsx
+++ src/routes/RootNavigator.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login.page";
+import { Layout } from "../pages/Layout.page";
 import SupplyMetrics from "../pages/SupplyMetrics.page";
 
 const RootNavigator = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Navigate replace to='/login' />} />
-        <Route path='/supply-metrics' element={<SupplyMetrics />} />
-      </Routes>
+        <Routes>       
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Navigate replace to='/login' />} />
+        </Routes>
+
+        <Layout>
+          <Routes>       
+            <Route path='/supply-metrics' element={<SupplyMetrics />} />
+          </Routes>
+        </Layout>
     </BrowserRouter>
   );
 };
