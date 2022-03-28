213eabcefe902c253027c43d34a67b62ca094dde
Vishal Grover <vishal_grover@crgl-thirdparty.com>
2022-03-28 12:59:12 +0530

    Updated LabourMetrics

--- src/components/settings/config-lead/LabourMetrics.component.tsx
+++ src/components/settings/config-lead/LabourMetrics.component.tsx
@@ -16,16 +16,14 @@ import {
   GridColDef,
   GridColumnHeaderParams,
   GridRenderCellParams,
+  GridRowId,
   GridSelectionModel,
-  MuiBaseEvent,
-  MuiEvent,
 } from "@mui/x-data-grid";
 import { CHILD_LABOUR_METRICS_ARR } from "constants/settings.constants";
 import { useSelector } from "react-redux";
 import { RootState, useAppDispatch } from "state/store";
 import { useEffect } from "react";
 import { childLabourMetricsActionCreator } from "state/actions/settings/config-lead/configLead.action";
-import { ChildLabourMetricsI } from "model/settings.model";
 
 const DataGridContainer = styled(DataGrid)(({ theme }) => ({
   margin: "1% 2rem",
@@ -120,76 +118,113 @@ const DataGridContainer = styled(DataGrid)(({ theme }) => ({
   },
 }));
 
-const MetricsHeader = (params: GridColumnHeaderParams) => {
-  return (
-    <Stack>
-      <Typography className='metricHeader'>Metric</Typography>
-      <Typography variant='subtitle1' className='metricHeader'>
-        Reporting on these metrics is mandatory
-      </Typography>
-    </Stack>
-  );
-};
-
-const MetricName = (params: GridRenderCellParams) => {
-  return (
-    <Typography className='metricName' variant='body2'>
-      {params.row.metricName}
-    </Typography>
-  );
-};
-
-const RangeLow = (params: GridRenderCellParams) => {
-  return <Typography className='range'>0</Typography>;
-};
-
-const RangeHigh = (params: GridRenderCellParams) => {
-  return <Typography className='range'>100</Typography>;
-};
-
-const DataCollectionMethod = (params: GridRenderCellParams) => {
-  const handleChange = (event: any) => {
-    console.log("🚀 ~ file: LabourMetrics.component.tsx ~ line 151 ~ DataCollectionMethod ~ event", event);
-  };
-  const value = params.row.dataCollectionMethod;
-  return (
-    <Select variant='outlined' value={value} onChange={handleChange}>
-      <MenuItem value={"screen"}>On-screen Input</MenuItem>
-      <MenuItem value={"source"}>Pull from source system</MenuItem>
-    </Select>
-  );
-};
-
-const columns: GridColDef[] = [
-  {
-    field: "metricName",
-    headerName: "Metrics",
-    renderHeader: (params) => MetricsHeader(params),
-    renderCell: (params) => MetricName(params),
+const AccordionContainer = styled(Accordion)(({ theme }) => ({
+  "& .metricsSize": {
+    width: "6rem",
+    backgroundColor: `${theme.palette.grey[700]}`,
+    color: "white",
+    textAlign: "center",
+    borderRadius: "16px",
+    marginRight: "16px",
   },
-  { field: "unit", headerName: "Units of Measure", align: "center" },
-  {
-    field: "rangeLow",
-    headerName: "Range Low",
-    renderCell: (params) => RangeLow(params),
-    editable: true,
-  },
-  {
-    field: "rangeHigh",
-    headerName: "Range High",
-    renderCell: (params) => RangeHigh(params),
-    editable: true,
-  },
-  {
-    field: "dataCollectionMethod",
-    headerName: "Data Collection Method",
-    renderCell: (params) => DataCollectionMethod(params),
-  },
-];
+}));
 
 const LabourMetrics = () => {
   const childLabourData = useSelector((state: RootState) => state.configLead.childLabourMetrics);
   const dispatch = useAppDispatch();
+
+  const MetricsHeader = (params: GridColumnHeaderParams) => {
+    return (
+      <Stack>
+        <Typography className='metricHeader'>Metric</Typography>
+        <Typography variant='subtitle1' className='metricHeader'>
+          Reporting on these metrics is mandatory
+        </Typography>
+      </Stack>
+    );
+  };
+
+  const MetricName = (params: GridRenderCellParams) => {
+    return (
+      <Typography className='metricName' variant='body2'>
+        {params.row.metricName}
+      </Typography>
+    );
+  };
+
+  const RangeLow = (params: GridRenderCellParams) => {
+    return <Typography className='range'>{params.row.rangeLow}</Typography>;
+  };
+
+  const RangeHigh = (params: GridRenderCellParams) => {
+    return <Typography className='range'>{params.row.rangeHigh}</Typography>;
+  };
+
+  function updateFieldData(
+    id: GridRowId,
+    value: string | number | boolean | object | Date | null | undefined,
+    field: string
+  ) {
+    // Find the object such that it can be editted.
+    let edittedObj = childLabourData.find((obj) => obj.id === +id);
+    if (edittedObj) {
+      if (value) edittedObj = { ...edittedObj, [field]: value };
+
+      // Find object index such that the updated object can be replaced by previous object.
+      const objIndex = childLabourData.findIndex((obj) => obj.id === +id);
+      const tempArr = [...childLabourData];
+      tempArr.splice(objIndex, 1, edittedObj);
+
+      // Dispatch the updated array to Redux
+      dispatch(dispatch(childLabourMetricsActionCreator(tempArr)));
+    }
+  }
+  const handleCellEdit = (params: GridCellEditCommitParams) => {
+    const { id, value, field } = params;
+    updateFieldData(id, value, field);
+  };
+
+  const DataCollectionMethod = (params: GridRenderCellParams) => {
+    const handleChange = (event: any) => {
+      const { name, value } = event.target;
+      updateFieldData(name, value, "dataCollectionMethod");
+    };
+    const value = params.row.dataCollectionMethod;
+    const name = `${params.row.id}`;
+    return (
+      <Select variant='outlined' value={value} onChange={handleChange} name={name}>
+        <MenuItem value={"screen"}>On-screen Input</MenuItem>
+        <MenuItem value={"source"}>Pull from source system</MenuItem>
+      </Select>
+    );
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "metricName",
+      headerName: "Metrics",
+      renderHeader: (params) => MetricsHeader(params),
+      renderCell: (params) => MetricName(params),
+    },
+    { field: "unit", headerName: "Units of Measure", align: "center" },
+    {
+      field: "rangeLow",
+      headerName: "Range Low",
+      renderCell: (params) => RangeLow(params),
+      editable: true,
+    },
+    {
+      field: "rangeHigh",
+      headerName: "Range High",
+      renderCell: (params) => RangeHigh(params),
+      editable: true,
+    },
+    {
+      field: "dataCollectionMethod",
+      headerName: "Data Collection Method",
+      renderCell: (params) => DataCollectionMethod(params),
+    },
+  ];
 
   const handleCheckboxSelect = (selectionModel: GridSelectionModel, details: GridCallbackDetails) => {
     // selectionModel.length > 0 ? setIsUpdateBtnDisabled(false) : setIsUpdateBtnDisabled(true);
@@ -200,28 +235,14 @@ const LabourMetrics = () => {
     dispatch(childLabourMetricsActionCreator(CHILD_LABOUR_METRICS_ARR));
   }, [dispatch]);
 
-  const handleCellEdit = (params: GridCellEditCommitParams) => {
-    type K = keyof ChildLabourMetricsI;
-    console.log("🚀 ~ file: LabourMetrics.component.tsx ~ line 207 ~ LabourMetrics ~ params", params);
-
-    const { id, field, value } = params;
-    const edittedObj = childLabourData.find((obj) => obj.id === id);
-    if (edittedObj) {
-      const objIndex = childLabourData.findIndex((obj) => obj.id === id);
-      if (value) {
-        edittedObj[field as keyof ChildLabourMetricsI] = value as string;
-      }
-      const tempArr = childLabourData.splice(1, objIndex, edittedObj);
-      dispatch(dispatch(childLabourMetricsActionCreator(tempArr)));
-    }
-  };
-
   return (
-    <Accordion sx={{ width: "100%" }}>
+    <AccordionContainer sx={{ width: "100%" }}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ width: "100%" }}>
           <Typography variant='body2'>Labour / Child Labour</Typography>
-          <Typography>8 Metrics</Typography>
+          <Typography className='metricsSize' variant='body2'>
+            8 Metrics
+          </Typography>
         </Stack>
       </AccordionSummary>
       <AccordionDetails>
@@ -235,7 +256,7 @@ const LabourMetrics = () => {
           onCellEditCommit={handleCellEdit}
         />
       </AccordionDetails>
-    </Accordion>
+    </AccordionContainer>
   );
 };
 
